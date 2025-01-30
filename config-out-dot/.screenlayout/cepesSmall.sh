50701dcb327c8c487d64be4580a24fc8440adf87
Davi-K-Silva <davi.kds.01@gmail.com>
2025-01-29 23:15:47 -0300

    :boom: chore: clean bspwm branch remains

--- config-out-dot/.screenlayout/cepesSmall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-A-0 --off --output DVI-I-2-2 --mode 1920x1080 --pos 1920x240 --rotate right --output DVI-I-1-1 --mode 1920x1080 --pos 0x0 --rotate normal

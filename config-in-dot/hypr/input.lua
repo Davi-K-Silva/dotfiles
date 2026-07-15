20e1ca1f21d8976d1d584e502c08997624a0a161
Davi-K-Silva <davi.kds.01@gmail.com>
2026-07-15 15:36:34 -0300

    Add different keyboard devices

--- config-in-dot/hypr/input.lua
+++ config-in-dot/hypr/input.lua
@@ -4,7 +4,6 @@
 
 hl.config({
   input = {
-    kb_layout = "br",
     kb_variant = "",
     kb_model = "",
     kb_options = "",
@@ -19,4 +18,19 @@ hl.config({
   cursor = {
     no_hardware_cursors = true,
   },
+})
+
+hl.device({
+  name = "at-translated-set-2-keyboard",
+  kb_layout = "br",
+})
+
+hl.device({
+  name = "dell-computer-corp-dell-universal-receiver",
+  kb_layout = "br",
+})
+
+hl.device({
+  name = "keychron-keychron-k2-he-keyboard",
+  kb_layout = "us",
 })
